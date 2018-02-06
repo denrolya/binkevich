963074d2db22fad3e7100d2d6ac71e259a52e39c
drolya <denrolya@gmail.com>
2018-02-06 21:22:30 +0100

    feat(product image fixture): implement fixtures for product images

--- src/AppBundle/DataFixtures/ORM/Dev/DataLoader.php
+++ src/AppBundle/DataFixtures/ORM/Dev/DataLoader.php
@@ -3,6 +3,8 @@ namespace AppBundle\DataFixtures\ORM\Dev;
 
 use Faker\Factory;
 use Hautelook\AliceBundle\Doctrine\DataFixtures\AbstractLoader;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class DataLoader extends AbstractLoader
 {
@@ -12,10 +14,26 @@ class DataLoader extends AbstractLoader
     public function getFixtures()
     {
         return  [
-            __DIR__ . '/post.yml',
+//            __DIR__ . '/post.yml',
             __DIR__ . '/user.yml',
 //            __DIR__ . '/slider_picture.yml',
             __DIR__ . '/ring.yml',
+            __DIR__ . '/product_image.yml'
         ];
+    }
+
+    public function uploadedFile()
+    {
+        $faker = Factory::create();
+        $file = $faker->image(__DIR__ . '/../../../../../web/uploads/products/test', 640, 480, 'cats');
+
+        return $uploadedFile = new UploadedFile(
+            $file,
+            $file,
+            mime_content_type($file),
+            filesize($file),
+            null,
+            true
+        );
     }
 }
