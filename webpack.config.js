9e444856fb40393a6411fb18a099e941c51074ad
drolya <denrolya@gmail.com>
2018-02-10 16:07:35 +0100

    refactor(structure): implement basic project structure

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,11 @@ Encore
     .setPublicPath('/build')
 
     // will create web/build/app.js and web/build/app.css
-    .addEntry('common', './assets/js/common.jsx')
+    .addEntry('common', './assets/js/common/app.jsx')
     .addEntry('index', './assets/js/index/app.jsx')
-    .addEntry('product', './assets/js/product/app.jsx')
+    .addEntry('product_list', './assets/js/product/List/app.jsx')
+    .addEntry('product_collection', './assets/js/product/Collection/app.jsx')
+    .addEntry('product_view', './assets/js/product/View/app.jsx')
 
     // allow sass/scss files to be processed
     .enableSassLoader()
