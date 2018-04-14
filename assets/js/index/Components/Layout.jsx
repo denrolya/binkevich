e99821d9170b25fc6423a0dbecf2c097d4604a90
drolya <denrolya@gmail.com>
2018-04-14 18:26:53 +0200

    refactor(index layout): add header to index

--- assets/js/index/Components/Layout.jsx
+++ assets/js/index/Components/Layout.jsx
@@ -1,49 +1,16 @@
 import React, {Component} from 'react';
 import fetch from 'isomorphic-fetch';
-import {Button} from 'reactstrap';
+import HeaderLight from '../../common/Components/HeaderLight';
 
 export default class Layout extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            blogPosts: []
-        }
     }
 
     render() {
         return (
             <div>
-                <h1>Big big table!</h1>
-
-                <p>
-                    <Button color="primary">primary</Button>
-                    <Button color="secondary">secondary</Button>
-                    <Button color="success">success</Button>
-                    <Button color="info">info</Button>
-                    <Button color="warning">warning</Button>
-                    <Button color="danger">danger</Button>
-                    <Button color="link">link</Button>
-                </p>
-
-                <table>
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Title</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    {this.state.rings && this.state.rings.map((ring, i) => {
-                        return (
-                            <tr key={i}>
-                                <td>{ring.id} <Button color="danger">Test</Button></td>
-                                <td>{ring.name}</td>
-                            </tr>
-                        )
-                    })}
-                    </tbody>
-                </table>
+                <HeaderLight/>
             </div>
         );
     }
