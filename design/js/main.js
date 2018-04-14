d83d1957fde399822bad86ae1c256db109fe4f02
drolya <denrolya@gmail.com>
2018-04-14 17:03:47 +0200

    style(designs): import provided designs

---
+++ design/js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+    var $mobileMenu = $('.mobile-menu');
+
+    $('.close-mobile-menu').click(function(){
+        $mobileMenu.removeClass('open');
+    });
+    $('.menu-btn').click(function(){
+        $mobileMenu.addClass('open');
+    });
+
+    $('.have-sub-mobile-menu').click(function(){
+
+        $(this).next('ul').slideToggle();
+
+    });
+});
