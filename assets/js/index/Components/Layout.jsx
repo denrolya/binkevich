9c7e9137103d304a95d00c3039a6322e93bf9fcf
drolya <denrolya@gmail.com>
2018-04-14 19:00:41 +0200

    refactor(index): add home white carousel section

--- assets/js/index/Components/Layout.jsx
+++ assets/js/index/Components/Layout.jsx
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-fetch';
 import HeaderLight from '../../common/Components/HeaderLight';
 import Footer from '../../common/Components/Footer';
 import HomeProductBlock from './HomeProductBlock';
+import HomeWhiteCarousel from './HomeWhiteCarousel';
 import HomeTopBlock from "./HomeTopBlock";
 
 export default class Layout extends Component {
@@ -16,6 +17,7 @@ export default class Layout extends Component {
                 <HeaderLight/>
                 <HomeTopBlock/>
                 <HomeProductBlock/>
+                <HomeWhiteCarousel/>
                 <Footer/>
             </div>
         );
