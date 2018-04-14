164ad543ad1b97789419fcdb70e95b101f4f70e7
Artem Zamarajev <azamaraj@mail.uni-mannheim.de>
2018-04-14 18:57:29 +0200

    first section added

--- assets/js/index/Components/Layout.jsx
+++ assets/js/index/Components/Layout.jsx
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-fetch';
 import HeaderLight from '../../common/Components/HeaderLight';
 import Footer from '../../common/Components/Footer';
 import HomeProductBlock from './HomeProductBlock';
+import HomeTopBlock from "./HomeTopBlock";
 
 export default class Layout extends Component {
     constructor(props) {
@@ -13,6 +14,7 @@ export default class Layout extends Component {
         return (
             <div>
                 <HeaderLight/>
+                <HomeTopBlock/>
                 <HomeProductBlock/>
                 <Footer/>
             </div>
