3b97beaabfc4b2f4f8a2153bf1a6af6fdbadb866
Denis Rolya <drolya@passionfive.de>
2018-04-28 20:03:23 +0200

    feat(auth handler): redirect admin to orders list

---
+++ src/AppBundle/Service/AuthenticationHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Services;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class AuthenticationHandler implements AuthenticationSuccessHandlerInterface
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        $url = $this
+            ->container
+            ->get('router')
+            ->generate('admin_app_order_list');
+
+        return new RedirectResponse($url);
+    }
+}
