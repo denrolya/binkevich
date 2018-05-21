5742acbbb1e9079ba9b9ea178efbddcbd3e83973
Denis Rolya <drolya@passionfive.de>
2018-05-21 19:45:19 +0200

    refactor(header): move out mobile header to separate component

--- assets/js/common/Components/HeaderComponent.jsx
+++ assets/js/common/Components/HeaderComponent.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import HeaderMobile from './HeaderMobile';
 
 export default class Header extends Component {
     constructor(props) {
@@ -8,63 +9,8 @@ export default class Header extends Component {
     render() {
         return (
             <header className={'container d-flex ' + ((this.props.dark === true) ? 'bg-transparent position-absolute' : 'bg-white')}>
-                <div className="mobile-menu">
-                    <div className="wrap-els">
-                        <div className="d-flex">
-                            <a className="logo marg-auto" href="/"></a>
-                            <button type="button" className="close-mobile-menu">&#10006;</button>
-                        </div>
 
-                        <ul>
-                            <li>
-                                <a href="/">HOME</a>
-                            </li>
-                            <li>
-                                <a href="" className="have-sub-mobile-menu">JEWELLERY</a>
-                                <ul>
-                                    <li className="have-sub-mobile-menu">
-                                        <a href="/categories" className="have-sub-mobile-menu">CATEGORIES</a>
-                                        <ul>
-                                            <li>
-                                                <a href="/categories/ring">RINGS</a>
-                                            </li>
-                                            <li>
-                                                <a href="/categories/earring">EARRINGS</a>
-                                            </li>
-                                            <li>
-                                                <a href="/categories/bangle">BANGLES</a>
-                                            </li>
-                                            <li>
-                                                <a href="/categories/pendant">PENDANTS</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="/collections" className="have-sub-mobile-menu">COLLECTIONS</a>
-                                        <ul>
-                                            {this.props.collections && this.props.collections.map((collection, i) => {
-                                                return (
-                                                    <li key={i}>
-                                                        <a href={'/collections/' + collection.slug}>{collection.name}</a>
-                                                    </li>
-                                                );
-                                            })}
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="/lookbook">LOOKBOOK</a>
-                            </li>
-                            <li>
-                                <a href="/bespoke">BESPOKE</a>
-                            </li>
-                            <li>
-                                <a href="/contact">CONTACT</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <HeaderMobile collections={this.props.collections}></HeaderMobile>
 
                 <button type="button" className="menu-btn">
                     <span></span>
@@ -72,9 +18,7 @@ export default class Header extends Component {
                     <span></span>
                 </button>
 
-
                 <a className="logo" href="/"></a>
-
 
                 <ul className="menu-list marg-l-auto pad-t-30">
                     <li className="item first">
