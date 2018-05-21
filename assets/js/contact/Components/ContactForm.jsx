6a84846b40a4c9ec4fc84125eb109a499f0525c0
Denis Rolya <drolya@passionfive.de>
2018-05-21 20:04:46 +0200

    refactor(contact form): refactor contact form page layout

--- assets/js/contact/Components/ContactForm.jsx
+++ assets/js/contact/Components/ContactForm.jsx
@@ -2,86 +2,62 @@ import React, {Component} from 'react';
 
 const attach = require('../../../img/attach-file-icon.png');
 
-export default class ContactForm
-    extends Component {
+export default class ContactForm extends Component {
 
     render() {
         return (
-            <div>
-                <section className="contact-form">
-                    <div className="container bg-white">
-                        <div className="row">
-                            <div className="col-12 custom-pad">
-                                <h2>CONTACT</h2>
-                                <p>
-                                    If you’re interested in our collections or require our bespoke services
-                                    bringing your ideas to life, please do not hesitate to message
-                                    us and we will get back to you as soon as possible.
-                                </p>
-                            </div>
-                        </div>
-                        <form className="row" method="post">
-                            <div className="col-12 col-xl-6 custom-pad__l d-flex flex-wrap">
-                                <label>
-                                    NAME
-                                    <input type="text" name="name"/>
-                                </label>
-                                <label>
-                                    EMAIL
-                                    <input type="text" name="email"/>
-                                </label>
-                                <label>
-                                    PHONE NUMBER
-                                    <input type="text" name="phone"/>
-                                </label>
-                            </div>
-                            <div className="col-12 col-xl-6 custom-pad__r d-flex flex-wrap">
-                                <label>
-                                    MESSAGE
-                                    <textarea name="message" id=""></textarea>
-                                </label>
-                                <div
-                                    className="d-flex align-items-start w-100 flex-wrap flex-lg-nowrap flex-lg-nowrap">
-                                    <label className="file-label d-flex align-items-center">
-                                        <input type="file" name="attach-file" id="attach-file-contact-form"
-                                               multiple="multiple"/>
-                                        <img src={attach} alt="attach-file-icon"/>
-                                        <span className="d-block">
-                            <span>Attach your design</span>
-                            <span id="files-length"><b>0</b> files attached</span>
-                        </span>
-                                    </label>
-                                    <button type="button" className="btn align-self-center" data-toggle="modal"
-                                            data-target="#successModal">
-                                        SEND MESSAGE
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </section>
-
-                <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-content">
-                            <h2 id="successModalLabel">
-                                Your message
-                                has been sent
-                            </h2>
-                            <p>We’ll be in touch as soon as possible.</p>
-                            <button type="button" class="btn">back home</button>
+            <section className="contact-form">
+                <div className="container bg-white">
+                    <div className="row">
+                        <div className="col-12 custom-pad">
+                            <h2>CONTACT</h2>
+                            <p>
+                                If you’re interested in our collections or require our bespoke services
+                                bringing your ideas to life, please do not hesitate to message
+                                us and we will get back to you as soon as possible.
+                            </p>
                         </div>
                     </div>
+                    <form className="row" method="post">
+                        <div className="col-12 col-xl-6 custom-pad__l d-flex flex-wrap">
+                            <label>
+                                NAME
+                                <input type="text" name="name"/>
+                            </label>
+                            <label>
+                                EMAIL
+                                <input type="text" name="email"/>
+                            </label>
+                            <label>
+                                PHONE NUMBER
+                                <input type="text" name="phone"/>
+                            </label>
+                        </div>
+                        <div className="col-12 col-xl-6 custom-pad__r d-flex flex-wrap">
+                            <label>
+                                MESSAGE
+                                <textarea name="message" id=""></textarea>
+                            </label>
+                            <div
+                                className="d-flex align-items-start w-100 flex-wrap flex-lg-nowrap flex-lg-nowrap">
+                                <label className="file-label d-flex align-items-center">
+                                    <input type="file" name="attach-file" id="attach-file-contact-form"
+                                           multiple="multiple"/>
+                                    <img src={attach} alt="attach-file-icon"/>
+                                    <span className="d-block">
+                                        <span>Attach your design</span>
+                                        <span id="files-length"><b>0</b> files attached</span>
+                                    </span>
+                                </label>
+                                <button type="button" className="btn align-self-center" data-toggle="modal"
+                                        data-target="#successModal">
+                                    SEND MESSAGE
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-
-                <script type="text/javascript" src="js/main.js"></script>
-            </div>
+            </section>
         );
     }
 }
--- assets/js/contact/Components/Layout.jsx
+++ assets/js/contact/Components/Layout.jsx
@@ -10,6 +10,26 @@ export default class Layout extends Component {
             <div className="gradient-page">
                 <Header/>
                 <ContactForm/>
+
+                {/* TODO: Refactor */}
+                <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-content">
+                            <h2 id="successModalLabel">
+                                Your message
+                                has been sent
+                            </h2>
+                            <p>We’ll be in touch as soon as possible.</p>
+                            <button type="button" class="btn">back home</button>
+                        </div>
+                    </div>
+                </div>
+
                 <Footer/>
             </div>
         )
