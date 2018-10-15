174086b4c27c02ec0e44925dcd58399ff24db4d9
Den Rolya <denes@codekunst.com>
2018-10-15 14:10:00 +0200

    refactor(eslint): fix eslint errors

--- assets/js/common.jsx
+++ assets/js/common.jsx
@@ -1,39 +1,37 @@
-// import 'bootstrap/dist/css/bootstrap.css';
-
 const routes = require('../../web/js/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
 Routing.setRoutingData(routes);
-// import './../../css/main.css';
 
 // TODO: Refactor
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
     const $mobileMenu = $('.mobile-menu');
 
-    $('.close-mobile-menu').click(function(){
+    $('.close-mobile-menu').click(function () {
         $mobileMenu.removeClass('open');
     });
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $mobileMenu.addClass('open');
     });
 
-    $('.have-sub-mobile-menu').click(function(){
+    $('.have-sub-mobile-menu').click(function () {
         $(this).next('ul').slideToggle();
     });
 });
 
-window.addEventListener("load", function(){
+window.addEventListener('load', function () {
     window.cookieconsent.initialise({
-        "palette": {
-            "popup": {
-                "background": "#ffffff",
-                "text": "#000000"
+        'palette': {
+            'popup': {
+                'background': '#ffffff',
+                'text': '#000000'
             },
-            "button": {
-                "background": "#9fc6c6",
-                "text": "#ffffff"
+            'button': {
+                'background': '#9fc6c6',
+                'text': '#ffffff'
             }
         },
-        "theme": "edgeless",
-        "position": "bottom-right"
-    })});
+        'theme': 'edgeless',
+        'position': 'bottom-right'
+    })
+});
